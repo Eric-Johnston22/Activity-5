de7b95d559dfa6a0dc478538ee34e72a04035026
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-08-03 15:28:56 -0400

    basic read file and open file dialog

--- Activity 5/Form1.Designer.cs
+++ Activity 5/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.tbx_display_file = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(219, 173);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(75, 23);
+            this.btn_read.TabIndex = 0;
+            this.btn_read.Text = "Read file";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // tbx_display_file
+            // 
+            this.tbx_display_file.Location = new System.Drawing.Point(277, 230);
+            this.tbx_display_file.Multiline = true;
+            this.tbx_display_file.Name = "tbx_display_file";
+            this.tbx_display_file.Size = new System.Drawing.Size(234, 90);
+            this.tbx_display_file.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbx_display_file);
+            this.Controls.Add(this.btn_read);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.TextBox tbx_display_file;
     }
 }
 
--- Activity 5/Form1.cs
+++ Activity 5/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Activity_5
 {
@@ -15,6 +16,36 @@ namespace Activity_5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            StreamReader inputFile;
+            string fileData;
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                // Open the selected file
+                inputFile = File.OpenText(openFile.FileName);
+
+                // Loop to read entire file
+                while(!inputFile.EndOfStream)
+                {
+                    // Hold lower case data from file
+                    string lowerCase;
+                    fileData = inputFile.ReadToEnd();
+                    // Convert fileData to lower case
+                    lowerCase = fileData.ToLower();
+
+                    tbx_display_file.Text = fileData;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Operation cancelled");
+            }
         }
     }
 }
